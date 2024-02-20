3a8ea7c0b914f4127ad429bc32c7d4262d8ccad3
S-Gabrie1 <sergegabriel24@gmail.com>
2024-02-20 09:19:39 +0100

    sign in button css fixed

--- src/components/unauth-page/index.js
+++ src/components/unauth-page/index.js
@@ -5,8 +5,11 @@ import React from 'react'
 
 export default function UnauthPage() {
   return (
-    <div>
-        <button onClick={() => signIn('github')}>Sign In</button>
+    <div className=' flex h-screen  justify-center items-center' >
+      <div className = ' border  px-[1.5em] py-[0.5em] border-gray-100 cursor-pointer  flex justify-center items-center'>
+        <button  onClick={() => signIn('github')}>Sign In</button>
+
+      </div>
     </div>
   )
 }
